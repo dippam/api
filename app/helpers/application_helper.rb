a022649bd2e4f931738f283345bfc7f7a378fb66
Conail Stewart <conailstewart@gmail.com>
2016-04-06 11:42:54 -0400

    Remove unnecessary application helpers.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,29 +3,9 @@ module ApplicationHelper
     content_for :title, str
   end
 
-  def add_js(str)
-    content_for :js, javascript_include_tag(str)
-  end
-  
-  def site_title
-    Site.locate(request.subdomains.first).short_title
-  end
-
-  def site_tagline
-    Site.locate(request.subdomains.first).title
-  end
-
   def sortable(col, title = nil)
     title ||= col.titleize
-    css_class = (col == sort_col) ? "current #{sort_dir}" : nil
-    dir = col == sort_col && sort_dir == "asc" ? "desc" : "asc"
-    link_to title, {:sort => col, :dir => dir}, {:class => css_class}
-  end
-
-  def include_ie_css
-    m = request.env['HTTP_USER_AGENT'].match /MSIE (\d)/
-    return if  m.nil?
-    stylesheet_link_tag 'ie_general' if m[1].to_i < 9
-    stylesheet_link_tag 'ie' + m[1]
+    dir = col == sort_col && sort_dir == 'asc' ? 'desc' : 'asc'
+    link_to title, {sort: col, dir: dir}
   end
 end
